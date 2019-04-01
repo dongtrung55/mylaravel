61df937aec015f7a912c8bb98c2a6dcd6c0becfd
pham trung dong <dongtrung55@gmail.com>
2019-04-01 17:16:20 +0700

    update

--- app/GiaoVien.php
+++ app/GiaoVien.php
@@ -9,7 +9,7 @@ class GiaoVien extends Model
     //khai bao table de thao tac
     public $table = "tblcbgv";
     //khai bao de  laravel hiểu là không có 2 cột là create_at và update_at
-    public $timestamps = false;
+    public $timestamps = true;
     //Lấy ra các cột cần sử dụng
     public $fillable = ['id','name','birthDay','address','luongCung','luongThuong','luongPhat','luongLinhThuc'];
 }
